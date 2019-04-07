b9356cbced549dacdca0de77f2b2224f41d727d7
Erik Koerner <koerner@informatik.uni-leipzig.de>
2019-04-07 23:02:26 +0200

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -219,7 +219,7 @@ def read(*names, **kwargs):
 
 setup(
     name="thai-segmenter",
-    version="0.3.2",
+    version="0.3.3",
     license="MIT license",
     description="Thai tokenizer, POS-tagger and sentence segmenter.",
     long_description="{readme}\n{changelog}".format(
